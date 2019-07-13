4aa31c2ecc6d861bcf283e096acf453ee780c376
julioro <julio.rodriguezo@pucp.pe>
2019-07-13 13:31:04 -0500

    Crear bootdisk.img y seed.iso

--- CloudInit/crearQcow.sh
+++ CloudInit/crearQcow.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+REP="/var/lib/libvirt/images"
 IMG=$1
 INDEX=$2
-rm -rf $DIR/../Imagenes/$INDEX-$IMG.img
-#rm -rf $DIR/../Imagenes/$INDEX-cloud-config.img 
-cp $DIR/../Imagenes/000-$IMG.img $DIR/../Imagenes/$INDEX-$IMG.img
-qemu-img create -f qcow2 -b $DIR/../Imagenes/$INDEX-$IMG.img /var/lib/libvirt/images/$INDEX-$IMG-DISK.img
-echo "Creado /var/lib/libvirt/images/$INDEX-$IMG-DISK.img"
-cloud-localds /var/lib/libvirt/images/$INDEX-$IMG-CLOUD.iso $DIR/../Imagenes/$INDEX-cloud-config.txt
-echo "Creado /var/lib/libvirt/images/$INDEX-$IMG-CLOUD.iso"
+
+mkdir -p $REP/$INDEX/
+rm -rf $REP/$INDEX/*
+
+qemu-img create -f qcow2 -b $REP/$IMG $REP/$INDEX/boot-disk.img
+echo -e "Creado disco:\t$REP/$INDEX/boot-disk.img"
+genisoimage -output $REP/seed.iso -volid cidata -joliet -rock $DIR/../Imagenes/$INDEX/meta-data $DIR/../Imagenes/$INDEX/user-data
+echo -e "Creado iso:\t$DIR/../Imagenes/$INDEX/meta-data & user-data"
