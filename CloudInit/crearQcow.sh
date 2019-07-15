644103fe340ccc2a71fd4a7e0b7e8e093c7ab2f4
julioro <julio.rodriguezo@pucp.pe>
2019-07-15 11:51:00 -0500

    No funciona ga uwu

--- CloudInit/crearQcow.sh
+++ CloudInit/crearQcow.sh
@@ -4,7 +4,7 @@ REP="/var/lib/libvirt/images"
 IMG=$1
 INDEX=$2
 
-qemu-img create -f qcow2 -b $REP/$IMG $REP/$INDEX/boot-disk.img
-echo -e "Creado disco:\t$REP/$INDEX/boot-disk.img"
-genisoimage -output $REP/seed.iso -volid cidata -joliet -rock $DIR/../Imagenes/$INDEX/meta-data $DIR/../Imagenes/$INDEX/user-data
-echo -e "Creado iso:\t$DIR/../Imagenes/$INDEX/meta-data & user-data"
+sudo qemu-img create -f qcow2 -b $REP/$IMG-BKUP.img $REP/$INDEX/boot-disk.img
+sudo echo -e "Creado disco:\t$REP/$INDEX/boot-disk.img"
+sudo genisoimage -output $REP/$INDEX/seed.iso -volid cidata -joliet -rock $DIR/../Imagenes/$INDEX/meta-data $DIR/../Imagenes/$INDEX/user-data
+sudo echo -e "Creado iso:\t$DIR/../Imagenes/$INDEX/meta-data & user-data"
--- MainConf/main.py
+++ MainConf/main.py
@@ -6,19 +6,25 @@ import llenarTemplates as lT
 import os
 import subprocess
 
-index = "001"
 templatePath = "../Template/"
+
+hwTemplatePath = templatePath + "domainConfig.xml"
+name = "vmach"
+mem = "1024"
+cantCpu = "1"
+index = "001"
 REP="/var/lib/libvirt/images"
 img = "bionic-server-cloudimg-amd64"
 print("Construyendo cloud-config ...")
 userDataPath= templatePath + "user-data"
 metaDataPath= templatePath + "meta-data"
 
-os.system("mkdir -p ../Imagenes/{0}".format(index))
-os.system("mkdir -p {0}/$INDEX/".format(REP))
-os.system("rm -rf {0}/{1}/*".format(REP, index))
+os.system("clear")
+os.system("sudo mkdir -p ../Imagenes/{0}".format(index))
+os.system("sudo rm -rf {0}/{1}/".format(REP, index))
+os.system("sudo mkdir -p {0}/{1}/".format(REP, index))
 
-name = "vm"
+
 sshKeyArray = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZRz8XJV0n/0/XBtHgDxL6v32Zpo47pxFs4TtK+M3fIX2i0vaEtIr/BgjjOMVBgQbniUB/+vSUA/ViWgmgXwyOBW/Z5MQmMEDIBtzE5H7BryCCIhGqDQ5m8bFZkV5uByCMjBtlQH7mhnjxs3sImGb8k2VjknoG8xVbX6UO+QWkPuoLqECxmiz3kWMixd8gMkO6J5moAr3CqzBXT3IrzcXYUZmtlL9Qv+zvqvmzbZ7J6zn3TFI+sleTKpq/EDyHWcEFQWVpeG8/bWEvyEBsQ6WmohcqVUDB/J+n7Ga1qCecldRiib/f495oT+XQo9J86NqSR+IiBr8yKZDT69/5dpfr juliorod@juliorod"]
 fileArray = [{"encoding":"raw", "content":"Skrra este archivo gaa", "owner":"ubuntu", "path":"/", "permissions":"0777", "append":"true"}]
 userArray = [{"name":name}]
@@ -32,26 +38,33 @@ f.close()
 
 print("Ejecutando sudo ../CloudInit/crearQcow.sh {0} {1} ...".format(img, index))
 os.system("sudo bash ../CloudInit/crearQcow.sh {0} {1}".format(img, index))
-#print(cloudConf)
-print("Construyendo domainConfig.xml ...")
-hwTemplatePath = templatePath + "domainConfig.xml"
-name = "test"
-mem = "1024"
-cantCpu = "1"
-disksArray = [{'type':'disk', 'path':"{0}/{1}/boot-disk.img".format(REP, index), 'hdType':'hdc'}, {'type':'cdrom', 'path':'{0}/{1}/seed.iso'.format(REP, index), 'hdType':'hda'}]
-ifacesArray = [{'name':'enp2s0', 'type':'ethernet', 'mac':'26:c7:a9:96:a7:7a', 'targetDev':'tap0'}]
-xmlConfig = lT.xmlConfig(hwTemplatePath, name, mem, cantCpu, disksArray, ifacesArray)
-#print(xmlConfig)
-print("END")
-exit(0)
 
+print("Construyendo domainConfig.xml ...")
+#exit(0)
+
+
+disksArray = [{'device':'disk', 'path':"{0}/{1}/boot-disk.img".format(REP, index), 'hdType':'hda', 'driverType':'qcow2'}, {'device':'disk', 'path':'{0}/{1}/seed.iso'.format(REP, index), 'hdType':'hdb', 'driverType':'raw'}]
+
+ifacesArray = [{'name': 'tap0', 'type':'network', 'mac':'52:55:00:d1:55:10', 'targetDev':'tap0', 'modelType':'rtl8139'}]
+xmlConfig = lT.xmlConfig(hwTemplatePath, name, mem, cantCpu, disksArray, ifacesArray)
+
+os.system("sudo ip link del dev tap0")
+os.system("sudo ip tuntap add dev tap0 mode tap")
+os.system("sudo ip link set dev tap0 address 52:55:00:d1:55:01")
+os.system("sudo ip link set dev tap0 up")
+os.system("sudo ip link show tap0")
+
+print("*"*70)
+print("libvirt confiuration:")
+
+print(xmlConfig)
 
 conn = libvirt.open('qemu:///system')
 if conn == None:
     print('Failed to open connection to qemu:///system')
     exit(1)
 
-dom = conn.createXML(xmlConfig, 0)
+dom = conn.createXML(xmlConfig.replace("\n", ""), 0)
 if dom == None:
     print('Failed to create a domain from an XML definition.')
     exit(1)
