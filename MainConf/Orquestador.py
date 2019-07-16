4bb36b011dbeb2c91ad39ae3c5865ad7bf8c9d26
julioro <julio.rodriguezo@pucp.pe>
2019-07-15 20:59:10 -0500

    Base para nuevo main

--- MainConf/Orquestador.py
+++ MainConf/Orquestador.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 '''
 1. Crear maquina virtual
     - Mem (MiB)
@@ -12,10 +14,24 @@
 '''
 
 import sys
+import BootCloud as BC
+
+dictVM = {}
 
 
-def vmImgCloudInit():
-    mem = input('Espacio en memoria (MiB)')
-    cpus = input('Cantidad de CPUs: ')
-    print("Imagenes disponibles:")
-    imgAv
+def vmImgCloudInit(cant):
+    mem = "1024"
+    cpus = "1"
+    imgOp = 1
+    #mem = input('Espacio en memoria (MiB):\t')
+    #cpus = input('Cantidad de CPUs:\t')
+    for _ in range(cant):
+        index = str(len(dictVM) + 1)  
+        imgArr = ["bionic-server-cloudimg-amd64.img", "CentOS-7-x86_64-GenericCloud.qcow2"]
+        print("Imagenes disponibles:")
+        [print("{0}) {1}".format(i+1, imgArr[i])) for i in range (len(imgArr))]
+        #imgOp = int(input("Opcion:\t"))
+        flag = BC.bootImg(index, imgArr[imgOp-1], mem, cpus)
+        if flag: dictVM[index] = "on"
+
+vmImgCloudInit(4)
