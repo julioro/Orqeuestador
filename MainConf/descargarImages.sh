5f2db69a43073685a645b16f813626f5b26bc924
Julito Montoya <julio.rodriguezo@pucp>
2019-07-16 13:31:19 -0500

    El kernel de CentOS

--- MainConf/descargarImages.sh
+++ MainConf/descargarImages.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+if [ "$#" -ne 1 ]; then
+	echo "Especificar home del usuario"
+fi
 
 # Se crea la carpeta donde se guardara todo
-mkdir -p $1
-cd $1
+mkdir -p $1/images
+cd $1/images
 
 # Se descarga las imagenes de Ubuntu y CentOS
 wget https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img -O bionic-server-cloudimg-amd64.img
@@ -11,9 +14,9 @@ wget https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
 # Se descarga el kernel y initrd de CentOS y Ubuntu
 
 # Ubuntu
-wget https://cloud-images.ubuntu.com/bionic/current/unpacked/bionic-server-cloudimg-amd64-vmlinuz-generic -O vmlinuz-ubuntu
-wget https://cloud-images.ubuntu.com/bionic/current/unpacked/bionic-server-cloudimg-amd64-initrd-generic -O initrd-ubuntu
+cp $1/Orquestador/Kernel/vmlinuz-ubuntu $1/images/vmlinuz-ubuntu
+cp $1/Orquestador/Kernel/initrd-ubuntu $1/images/initrd-ubuntu
 
 # CentOS
-wget http://mirror.centos.org/centos/7/os/x86_64/images/pxeboot/vmlinuz -O vmlinuz-centos
-wget http://mirror.centos.org/centos/7/os/x86_64/images/pxeboot/initrd.img -O initrd-centos
+cp $1/Orquestador/Kernel/vmlinuz-centos $1/images/vmlinuz-centos
+cp $1/Orquestador/Kernel/initrd-centos $1/images/initrd-centos
